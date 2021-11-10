57af30ff1adfd469fb24985d72942f55eb602613
andreimek <nescioraj@gmail.com>
2021-11-10 10:22:19 +0100

    poprawki

--- servers.py
+++ servers.py
@@ -6,6 +6,10 @@ import re
 
 
 class Product:
+    # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą argumenty wyrażające nazwę produktu (typu str) i jego cenę (typu float) -- w takiej kolejności -- i ustawiającą atrybuty `name` (typu str) oraz `price` (typu float)
+    def __init__(self, name: str, price: float) -> None:
+        self.name = name
+        self.price = price
     def __init__(self, name: str, price: float):
         self.name: str = name
         self.price: float = price
@@ -14,13 +18,12 @@ class Product:
         hash_price = hash(self.price)
         return hash_name, hash_price
 
-    def __eq__(self, other):
-        return self.name == other.name, self.price == other.name
 
 
 class TooManyProductsFoundError(Exception):
     # Reprezentuje wyjątek związany ze znalezieniem zbyt dużej liczby produktów.
     pass
+
 
 class Server(ABC):
     n_max_returned_entries: int = 3
@@ -32,14 +35,16 @@ class Server(ABC):
         symbols = '^[a-zA-Z]{{{n}}}\\d{{2,3}}$'.format(n=n_letters)
         entries = []
         for x in self._get_products(n_letters):
-            if re.match(symbols,x.name):
+            if re.match(symbols, x.name):
                 entries.append(x)
             if len(entries) > Server.n_max_returned_entries:
                 raise TooManyProductsFoundError
-        return sorted(entries,key= lambda e:e.price)
+        return sorted(entries, key=lambda e: e.price)
+
     @abstractmethod
-    def _get_products_as_list(self, n_letters: int = 1) -> List[Product]:
+    def _get_products(self, n_letters: int = 1) -> List[Product]:
         raise NotImplementedError
+
 
 # FIXME: Każada z poniższych klas serwerów powinna posiadać:
 #   (1) metodę inicjalizacyjną przyjmującą listę obiektów typu `Product` i ustawiającą atrybut `products` zgodnie z typem reprezentacji produktów na danym serwerze,
@@ -52,20 +57,32 @@ class ListServer(Server):
         super().__init__(*args, **kwargs)
         self.products: List[Product] = products
 
-    def _get_products_as_list(self, n_letters: int = 1) -> List[Product]:
+    def _get_products(self, n_letters: int = 1) -> List[Product]:
         return self.products
 
 
 class MapServer(Server):
     def __init__(self, products: List[Product], *args, **kwargs) -> None:
         super().__init__(*args, **kwargs)
-        self.products: Dict[str, Product] = {p.name: p.price for p in products}
+        self.products: Dict[str, Product] = {p.name: p for p in products}
 
-    def _get_products_as_list(self, n_letters: int = 1) -> List[Product]:
+    def _get_products(self, n_letters: int = 1) -> List[Product]:
+
         return list(self.products.values())
+
 
 class Client:
     # FIXME: klasa powinna posiadać metodę inicjalizacyjną przyjmującą obiekt reprezentujący serwer
 
+    def __init__(self, server) -> None:
+        self.server: Server = server
+
     def get_total_price(self, n_letters: Optional[int]) -> Optional[float]:
-        raise NotImplementedError()
+        try:
+            entries = self.server.get_entries(n_letters)
+            sum = 0
+            for entry in entries:
+                sum += entry.price
+            return sum
+        except:
+            return None
